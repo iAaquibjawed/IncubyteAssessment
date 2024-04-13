747d16694191389e3016bb94ad6c88a85ad93750
aaquib <mallickaaquib@gmail.com>
2024-04-13 20:04:45 +0530

    added the function to detect the delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,5 +3,9 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i
   end
+
+  def self.detect_delimiter(numbers)
+    numbers[0] == '/' && numbers[1] == '/' ? numbers[2] : /[,\n]/
+  end
 end
   
