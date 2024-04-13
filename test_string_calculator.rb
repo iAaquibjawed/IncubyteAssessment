0a99f345132d7a32dc729c09ed590209416f5ece
aaquib <mallickaaquib@gmail.com>
2024-04-13 19:59:22 +0530

    fix the issue for the class

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -1,7 +1,13 @@
 require 'minitest/autorun'
 require_relative 'string_calculator'
 
-def test_empty_string
-  assert_equal(0, StringCalculator.add(""))
+class TestStringCalculator < Minitest::Test
+    def test_empty_string
+      assert_equal(0, StringCalculator.add(""))
+    end
+  
+    def test_single_number
+      assert_equal(1, StringCalculator.add("1"))
+    end
 end
   
